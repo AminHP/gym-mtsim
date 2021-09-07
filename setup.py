67f5fb6fb7018b650d94747e41142dbb1b8696b3
AminHP <mdan.hagh@gmail.com>
2021-09-08 00:03:37 +0430

    Release v1.0.0

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gym_mtsim',
+    version='1.0.0',
+    packages=find_packages(),
+
+    author='AminHP',
+    author_email='mdan.hagh@gmail.com',
+
+    install_requires=[
+        'gym>=0.19.0',
+        'numpy>=1.19.5',
+        'scipy>=1.7.1',
+        'pandas>=1.3.1',
+        'matplotlib>=3.4.2',
+        'plotly>=5.3.1',
+        'nbformat>=5.1.3',
+        'pathos>=0.2.8',
+        'MetaTrader5>=5.0.35',
+    ],
+
+    package_data={
+        'gym_mtsim': ['data/*.pkl']
+    }
+)
